f2e785b8ca6f9d4a4b80ed71c56996264149bdf7
thesave <Thesave86@gmail.com>
2016-08-19 10:18:13 +0200

    updated for the new version of PrimsJS

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-var Prism = require('prismjs');
-var languages = require('prism-languages');
-var path = require('path');
+Prism = require( 'prismjs' );
+var languages = require( 'prism-languages' );
+var path = require( 'path' );
+var cheerio = require( 'cheerio' );
 
-var prismCSS = require.resolve('prismjs/themes/prism.css');
+var prismDir = require.resolve( "prismjs/prism.js" );
 
 var DEFAULT_LANGUAGE = 'markup';
 var MAP_LANGUAGES = {
-  'py': 'python',
-  'js': 'javascript',
-  'json': 'javascript',
-  'rb': 'ruby',
-  'csharp': 'cs',
-  'html': 'markup'
+	'py': 'python',
+	'js': 'javascript',
+	'json': 'javascript',
+	'rb': 'ruby',
+	'csharp': 'cs',
+	'html': 'markup'
 };
 
 var assets = {
-  assets: path.dirname(prismCSS),
-  css: [path.basename(prismCSS)]
+	assets: path.dirname( prismDir ),
+	css: [ "themes/prism-okaidia.css", "plugins/line-numbers/prism-line-numbers.css" ]
 };
 
 module.exports = {
-  book: assets,
-  ebook: assets,
-  blocks: {
+	book: assets,
+	ebook: assets,
+	hooks:{
+		'page' : function( page ){
+			$ = cheerio.load( page.content );
+			$( "pre > code" ).each( function() {
+				$( this ).parent().addClass( "line-numbers" );
+				$( this ).parent().addClass( $( this ).attr( "class" ) );
+				$( this ).text( $( this ).text() + '<!-- PRISM_LINENOS -->' );
+			});
+			page.content = $.html();
+			return page;
+		} 
+	},
+	blocks: {
     code: function(block) {
-      var highlighted = '';
+		  var highlighted = '';
 
       // Normalize language id
+
       var lang = block.kwargs.language || DEFAULT_LANGUAGE;
       lang = MAP_LANGUAGES[lang] || lang;
       if (!languages[lang]) lang = DEFAULT_LANGUAGE;
@@ -36,9 +50,36 @@ module.exports = {
         lang = 'markup';
       }
 
+      block.body = block.body.trim();
+
+      var line_numbers;
+      if ( block.body.indexOf( '<!-- PRISM_LINENOS -->' ) > -1 ){
+        block.body = block.body.substring( 0, 
+          block.body.indexOf( '<!-- PRISM_LINENOS -->' ) );
+        block.body = block.body.trim();
+        var linesNum = ( block.body.split('\n').length+1 );
+        var lines = new Array( linesNum );
+        lines = lines.join( '<span></span>' );
+        line_numbers = '<span class="line-numbers-rows" aria-hidden="true">' +
+          lines + '</span>';
+      }
+
+
+      // if ( block.body.indexOf( "\n" ) === 0 ){
+      //   block.body = block.body.substring( 1, block.body.length );
+      // }
+
+      // if ( block.body[ block.body.length ] === "\n" ){
+      //   block.body = block.body.substring( 0, block.body.length-2 );
+      // }
+
       try {
         // The process can fail (failed to parse)
-        highlighted = Prism.highlight(block.body, languages[lang]);
+        highlighted = Prism.highlight( block.body, languages[lang] );
+        if ( line_numbers !== undefined ){
+        	highlighted = highlighted + line_numbers;
+        }
+      
       } catch(e) {
         console.warn('Failed to highlight:');
         console.warn(e);
